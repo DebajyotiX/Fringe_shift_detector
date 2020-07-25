5173bc23e2c4a066064617182a10438d7ae1cf16
Debajyoti Majudmer <manofliquidsteel@gmail.com>
2020-07-26 00:46:37 +0530

    Added comments

--- fringe shift measurinator.py
+++ fringe shift measurinator.py
@@ -1,31 +1,42 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
-# import time
-# import sys
+from scipy.signal import find_peaks
 
-# Load an color image in grayscale
+# Load the "seemingly colorless but actually in RGB space" image in grayscale
 img = cv2.imread('ccd_image_compressed.png',0)
-blur = cv2.GaussianBlur(img,(5,5),0)
-for i in range(30):
-    blur = cv2.GaussianBlur(blur,(5,5),0)
 
-# a=np.max(blur)
-# blur=blur*128/a
-half1=blur[0:143,:]
-half2=blur[144:-1,:]
-# cv2.imshow('image upper',half1)
-cv2.imshow('image lower',half2)
+# Denoising
+denoised = cv2.GaussianBlur(img,(5,5),0)
+for i in range(30):
+    denoised = cv2.GaussianBlur(denoised,(5,5),0)
+
+# Seperating the two parts
+upper_half_image = denoised[0:143,:]
+lower_half_image = denoised[144:-1,:]
+
+# Displaying parts
+cv2.imshow('TOP PART',upper_half_image)
+cv2.imshow('BOTTOM PART',lower_half_image)
 
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
-lower = half2[66,:]
+lower_fringe_ = lower_half_image[23,:]
 print(np.shape(lower))
 x=np.array(range(536))
 # print(np.shape(x))
 plt.plot(x,lower)
+plt.xlabel("Pixel number")
+plt.ylabel("Relative brightness")
+
+#-----------peaks----------------#
+peaks, properties = find_peaks(lower,height=5, width=3)
+plt.plot(x[peaks], lower[peaks], "o", color='#ff6400')
+#-----------peaks----------------#
+
 plt.show()
+
 # plt.imshow(blur, cmap = 'gray', interpolation = 'bicubic')
 # # plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
 # plt.show()
