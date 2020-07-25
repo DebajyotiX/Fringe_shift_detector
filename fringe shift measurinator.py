168770a7e0ed0834a71f607c6ee094029346d694
Debajyoti Majudmer <manofliquidsteel@gmail.com>
2020-07-26 01:46:10 +0530

    Visualization cleaned and Phase shift remaining

--- fringe shift measurinator.py
+++ fringe shift measurinator.py
@@ -8,7 +8,7 @@ img = cv2.imread('ccd_image_compressed.png',0)
 
 # Denoising
 denoised = cv2.GaussianBlur(img,(5,5),0)
-for i in range(30):
+for i in range(30):   #I blur is 30 times. Its the sweet spot
     denoised = cv2.GaussianBlur(denoised,(5,5),0)
 
 # Seperating the two parts
@@ -22,21 +22,50 @@ cv2.imshow('BOTTOM PART',lower_half_image)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
-lower_fringe_ = lower_half_image[23,:]
-print(np.shape(lower))
-x=np.array(range(536))
-# print(np.shape(x))
-plt.plot(x,lower)
-plt.xlabel("Pixel number")
-plt.ylabel("Relative brightness")
+'''
+Calculation of the peaks,
+ by choosing two pixel line from the two parts of the image
+ this pixel lines runs horizontally and cuts the fringes perpendicularly
+'''
 
-#-----------peaks----------------#
-peaks, properties = find_peaks(lower,height=5, width=3)
-plt.plot(x[peaks], lower[peaks], "o", color='#ff6400')
-#-----------peaks----------------#
+lower_fringe_linear_sample = lower_half_image[23,:]
+upper_fringe_linear_sample = upper_half_image[123,:]
+x_pix_array = np.array(range(536))
+zero = np.zeros(536) # Zero array for plot purpose
+peaks, properties = find_peaks(lower_fringe_linear_sample,height=5, width=3)
+peaks2, properties2 = find_peaks(upper_fringe_linear_sample,height=5, width=3)
 
+'''
+Here I will find out the wavelength in units of pixels.
+I will calculate it from pixel to pixel distances of consecutive peaks
+'''
+
+
+#---------------- PLOT --------------------#
+plt.rcParams["figure.figsize"] = [10,5]
+fig, ax = plt.subplots(nrows=1, ncols=2,)
+ax[0].plot(x_pix_array,upper_fringe_linear_sample)
+ax[0].plot(x_pix_array[peaks2], upper_fringe_linear_sample[peaks2], "o", color='#ff6400',label="Peak")
+ax[0].plot(x_pix_array[peaks2], zero[peaks2], "o", color='#ff6400')
+ax[0].set_xlabel("Pixel number")
+ax[0].set_ylabel("Relative brightness")
+ax[0].set_title("Top Interference pattern")
+ax[0].legend()
+
+ax[1].plot(x_pix_array,lower_fringe_linear_sample)
+ax[1].plot(x_pix_array[peaks], lower_fringe_linear_sample[peaks], "o", color='#ff6400',label="Peak")
+ax[1].plot(x_pix_array[peaks], zero[peaks], "o", color='#ff6400')
+ax[1].set_xlabel("Pixel number")
+ax[1].set_ylabel("Relative brightness")
+ax[1].set_title("Bottom Interference pattern")
+ax[1].legend()
 plt.show()
+#---------------- PLOT OVER --------------------#
 
-# plt.imshow(blur, cmap = 'gray', interpolation = 'bicubic')
-# # plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-# plt.show()
+
+
+
+
+
+
+
